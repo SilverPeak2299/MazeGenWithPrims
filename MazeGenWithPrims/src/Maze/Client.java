6659da9bafd81492d9342ebda9c5b5127fe547e6
SilverPeak2299 <83009408+SilverPeak2299@users.noreply.github.com>
2022-01-12 12:43:06 +1100

    Update Client.java

--- MazeGenWithPrims/src/Maze/Client.java
+++ MazeGenWithPrims/src/Maze/Client.java
@@ -1,7 +1,7 @@
 package Maze;
 
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // A maze generator that uses a modified version of Prims algorithim - 1 represents a wall
         mazeGenerator Maze = new mazeGenerator();
         Maze.generateMaze();
         Maze.printMaze();
